d5b774419e92c1ea92d03f0f1cd6686abd72d856
MasterMagicStep <niklepa@mail.ru>
2024-08-27 13:43:27 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,19 @@
+#
+name = 'Nick'
+print(name)
+#
+age = int(24)
+print(age)
+#
+NewAge = int(age + 1)
+print(NewAge)
+#
+Is_Student = bool(True)
+print(Is_Student)
+# разделение
+print(str('---------------'))
+#
+print('Имя: ', name,)
+print('Текущий возраст: ', age,)
+print('Будущий возраст: ', NewAge,)
+print('Персона является студентом:', Is_Student,)
